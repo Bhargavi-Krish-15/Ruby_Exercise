b13236eb6b31fbd4994aacbadc54efbc33d0d2bb
Bhargavi-Krish-15 <bhargavikrish@outlook.com>
2021-12-30 12:20:56 +0530

    first commit

---
+++ exr1.rb
@@ -0,0 +1,11 @@
+
+#first program
+
+
+puts "hello galaxy!"
+puts "hello again"
+puts "I like typing this."
+puts "This is super fun."
+puts "Yay its printing..."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
---
+++ exr2.rb
@@ -0,0 +1,11 @@
+#comments and pond
+
+
+#this is a comment
+puts "This is a puts # statement"
+
+
+#my comment #lines will not get printed
+
+
+puts " But puts statement print my output"
